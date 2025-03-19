28a6a7eff54e8d2f36dfa8563008f4d0f7a15137
Björn <Bjoern.97@web.de>
2025-03-19 18:07:48 +0100

    files for first openGL testing

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "GLFW konnte nicht initialisiert werden!\n";
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL mit WSL", NULL, NULL);
+    if (!window) {
+        std::cerr << "Fenster konnte nicht erstellt werden!\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "GLEW konnte nicht initialisiert werden!\n";
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
