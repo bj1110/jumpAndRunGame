badbc522495e334c92be1c487f2fd4ed6294bb07
Björn <Bjoern.97@web.de>
2025-03-22 09:48:08 +0100

    Drew rectagle by introducing Index Array Buffer

--- main.cpp
+++ main.cpp
@@ -100,19 +100,32 @@ int main() {
 
     // Bis hier ist alles initialisierung gewesen
 
-    float positions[6]={
+    float positions[]={
         -0.5f, -0.5f, 
-        0.0f, 0.5f,
+        -0.5f, 0.5f,
+        0.5f, 0.5f,
         0.5f, -0.5f
     };
+
+    unsigned int indices[]={
+        0, 1, 2,
+        2, 3, 0
+    };
+
 
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6* sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 2* 6* sizeof(float), positions, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2* sizeof(float), 0); 
     glEnableVertexAttribArray(0);
+
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6* sizeof(unsigned int), indices, GL_STATIC_DRAW);
+
 
     auto [vertexShader, fragmentShader] = parseShader("../res/shaders/basic.shader");
 
@@ -122,8 +135,9 @@ int main() {
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3); 
-        
+        // glDrawArrays(GL_TRIANGLES, 0, 3); 
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
